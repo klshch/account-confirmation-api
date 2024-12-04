291b948866982a191d2dcc86c4540db68f75ca57
Anastasiia Klishch <147326209+klshch@users.noreply.github.com>
2024-12-04 15:51:23 +0200

    Add register and login routes

--- routes/confRoutes.js
+++ routes/confRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { sendActivationLink, activateAccount } = require('../controllers/confController');
+const { registerUser } = require('../controllers/regController');
+const { loginUser } = require('../controllers/logController');
 const router = express.Router();
 
 router.post('/send-activation-link', sendActivationLink);
 router.get('/activate-account', activateAccount);
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;
