a36c6a831df1218dd06696405513a4bdeeb139fe
Anastasiia Klishch <147326209+klshch@users.noreply.github.com>
2024-12-04 18:52:47 +0200

    Update rateLimiter.js for testing

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,8 +2,8 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
-  max: 3, 
-  message: 'Too many requests, please try again later',
+  max: 500, 
+  message: 'Too many requests, please try again later.',
 });
 
 module.exports = limiter;
